2c60f58673e638572356a0e3da9ff6c3d8cb7e89
NiThDevOps <nicolas.thomas2@gmail.com>
2025-09-05 18:58:27 +0200

    nouvelle tentative après un destroy/apply

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,7 +10,7 @@ terraform {
       version = ">= 2.25"
     }
   }
-  required_version = ">= 1.12.0" # dernière version "stable" disponible
+  required_version = ">= 1.12.0" # dernière version "stable" disponible  
 }
 
 # Fournisseur : AWS 
